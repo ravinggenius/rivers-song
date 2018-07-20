90b7ca70bd824fc164ac4cc9930b8d63b67ec60c
Thomas Ingram <code@ravinggenius.com>
2018-07-20 12:42:25 -0400

    Greet user with given name

--- app/code/RiversSong/Parrot/Block/Index.php
+++ app/code/RiversSong/Parrot/Block/Index.php
@@ -3,4 +3,7 @@
 namespace RiversSong\Parrot\Block;
 
 class Index extends \Magento\Framework\View\Element\Template {
+    public function getName() {
+        return $this->getRequest()->getParam('name');
+    }
 }
